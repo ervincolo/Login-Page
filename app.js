f0f53061e55a13c5b086d3a0aadb30c56982ee67
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2022-11-19 00:03:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById("login").addEventListener("click", getLogin);
+
+function getLogin () {
+	
+  	alert("Successfully logged in !")
+    console.log("Successfully logged in !");
+	
+}
+
+document.getElementById("register").addEventListener("click", getRegister);
+
+function getRegister ()  {
+	
+  alert("Successfully registered !");
+  console.log("Successfully registered !");
+
+}
